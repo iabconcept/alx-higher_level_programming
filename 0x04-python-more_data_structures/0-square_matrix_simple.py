5d737b8a3f7fb76896783e2b5c5b3c92c04d6ce1
Ibraheem Busari <ibraheemjob@gmail.com>
2022-09-28 12:57:03 -0700

    space

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3 
- def square_matrix_simple(matrix=[]): 
-     if not matrix: 
-         print() 
-     return [[item**2 for item in row] for row in matrix]
+#!/usr/bin/python
+def square_matrix_simple(matrix=[]):
+    if not matrix:
+        print()
+    return [[item**2 for item in row] for row in matrix]

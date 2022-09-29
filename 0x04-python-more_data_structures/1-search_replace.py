570e1f14aa5fca1566b3e5097cbe508e7366b7ff
Ibraheem Busari <ibraheemjob@gmail.com>
2022-09-29 12:49:37 -0700

    2

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,9 @@
-#!/usr/bin/python3 
- def search_replace(my_list, search, replace): 
-     return [find if find != search else replace for find in my_list]
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return new_list
